af865b02ea0feb8832d2968f49501c78e4d40c2f
Aakash <iakashman8653@gmail.com>
2024-04-09 22:32:57 +0530

    Applied basic versioning through URI path

--- src/main/java/com/akash/blog/controller/PostController.java
+++ src/main/java/com/akash/blog/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.akash.blog.controller;
 
 import com.akash.blog.payload.PostDto;
+import com.akash.blog.payload.PostDtoV2;
 import com.akash.blog.payload.PostResponse;
 import com.akash.blog.service.PostService;
 import com.akash.blog.utils.AppConstants;
@@ -15,10 +16,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+//@RequestMapping("/api/v1/posts")
 @Tag(
       name = "CRUD REST APIs for Post Resource"
 )
@@ -44,7 +46,7 @@ public class PostController {
             description = "Http status 201 CREATED"
     )
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping
+    @PostMapping("/api/v1/posts")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
@@ -58,7 +60,7 @@ public class PostController {
             responseCode = "200",
             description = "Http status 200 SUCCESS"
     )
-    @GetMapping
+    @GetMapping("/api/v1/posts")
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo",defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
@@ -69,18 +71,48 @@ public class PostController {
         return postService.getAllPosts(pageNo,pageSize,sortBy,sortDir);
     }
 
-    //get post by id REST API
+    //get post by id REST API Version 1
     @Operation(
-            summary = "Get Post By Id REST API",
+            summary = "Get Post By Id REST API V1",
             description = "Get post by Id REST API is used to show a particular post"
     )
     @ApiResponse(
             responseCode = "200",
             description = "Http status 200 SUCCESS"
     )
-    @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id){
+    @GetMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") long id){
         return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+
+    //get post by id REST API Version 2
+    @Operation(
+            summary = "Get Post By Id REST API V2",
+            description = "Get post by Id REST API is used to show a particular post"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 SUCCESS"
+    )
+    @GetMapping("/api/v2/posts/{id}")
+    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") long id){
+        PostDto postDto=postService.getPostById(id);
+        PostDtoV2 postDtoV2=new PostDtoV2();
+
+        postDtoV2.setId(postDto.getId());
+        postDtoV2.setTitle(postDto.getTitle());
+        postDtoV2.setDescription(postDto.getDescription());
+        postDtoV2.setContent(postDto.getContent());
+        postDtoV2.setComments(postDto.getComments());
+        postDtoV2.setCategoryId(postDto.getCategoryId());
+
+        List<String> tags=new ArrayList<>();
+        tags.add("Java");
+        tags.add("Spring Boot");
+        tags.add("AWS");
+        postDtoV2.setTags(tags);
+        return ResponseEntity.ok(postDtoV2);
     }
 
 
@@ -97,7 +129,7 @@ public class PostController {
             description = "Http status 200 SUCCESS"
     )
     @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public ResponseEntity<PostDto> updatePOst(@Valid @RequestBody PostDto postDto,@PathVariable(name = "id") long id){
         PostDto postResponse = postService.updatePost(postDto, id);
         return new ResponseEntity<>(postResponse,HttpStatus.OK);
@@ -116,7 +148,7 @@ public class PostController {
             description = "Http status 200 SUCCESS"
     )
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public ResponseEntity<String> deletePost(@PathVariable("id") long id){
         postService.deletePost(id);
         return new ResponseEntity<>("Post deleted successfully",HttpStatus.OK);
@@ -132,7 +164,7 @@ public class PostController {
             responseCode = "200",
             description = "Http status 200 SUCCESS"
     )
-    @GetMapping("/category/{id}")
+    @GetMapping("/api/v1/posts/category/{id}")
     public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable("id") Long categoryId){
         List<PostDto> postDtos = postService.getPostByCategory(categoryId);
         return ResponseEntity.ok(postDtos);
